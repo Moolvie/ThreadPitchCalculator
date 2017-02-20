527df921c1939e8c10f77ac5a5548b660a1d1c91
Moolvie <moolvie@gmail.com>
2017-02-20 15:27:59 -0500

    first commit

---
+++ ThreadPitchCalculator/src/threadpitchcalculator/ThreadPitchCalculator.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threadpitchcalculator;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Doug Enos
+ */
+public class ThreadPitchCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Declare Variables
+        
+        // Scanner class
+        Scanner keyboard = new Scanner(System.in);
+    }  
+}
